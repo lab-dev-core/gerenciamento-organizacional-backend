5069e2a3270e48f5d63e66b26c67b4a9572737aa
André Deitos <44508803+andred88@users.noreply.github.com>
2025-11-30 23:22:06 -0300

    Update WebConfig.java

--- src/main/java/com/gestaoformativa/config/WebConfig.java
+++ src/main/java/com/gestaoformativa/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://front.dev-core.online")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
